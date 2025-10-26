ff88d03ef7646c3e31ada4b2e8724518e8c94f55
Dinowerka <ars.mihailov2015@gmail.com>
2025-10-26 20:31:09 +0300

    Test

---
+++ Program.java
@@ -0,0 +1,14 @@
+public class Program{
+
+void main() {
+	System.out.println("Привт");
+	System.out.println(0b011);//запись в двоичной системе
+	System.out.println(0x0A);//шестнадцатеричная система
+	System.out.println(0X0A);
+	System.out.println(013); //восьмеричная система
+	System.out.println(132_131); //_ как символ для наглядного разделения числа
+	
+}
+}
+/*бара
+бери */
